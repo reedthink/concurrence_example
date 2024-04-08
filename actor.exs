6d53fc6d82a4cb8917b2cbaff7eb6cb0ca231095
jkjk <reedthink@qq.com>
2024-04-08 17:18:27 +0800

    添加actor示例

---
+++ actor.exs
@@ -0,0 +1,17 @@
+defmodule Talker do
+    def loop do
+        receive do
+            {:greet, name} -> IO.puts("Hello #{name}")
+            {:praise, name} -> IO.puts("#{name}, you're amazing")
+            {:celebrate, name, age} -> IO.puts("Here's to another #{age} years. #{name}")
+            {:shutdown} -> exit(:normal)
+        end
+        loop
+    end
+end
+
+pid = spawn(&Talker.loop/0)
+send(pid, {:greet, "Huey"})
+send(pid, {:praise, "Dewey"})
+send(pid, {:celebrate, "Louie", 16})
+# sleep(1000)
